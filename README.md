ae01531a78ccd757105d91194d382bdc3647365b
Tommy Lacke <tommy.lacke@external.atlascopco.com>
2020-11-27 23:39:49 +0100

    Initiate repo

--- README.md
+++ README.md
@@ -1,2 +1,117 @@
 # node-red-multi-sensor
-node red: Multi Sensor state machine
+Binary motion sensor and activation state machine node for Node-red.
+
+This node is a companion for any binary sensor (physical or virtual), storing all recieved sensors and their state and merge them into a single node.
+
+![node-appearance](assets/node-appearance.png "Node appearance")  
+**Fig. 1:** Node appearance
+
+<a name="installation"></a>
+## Installation
+
+<a name="installation_in_node-red"></a>
+### In Node-RED (preferred)
+* Via Manage Palette -> Search for "node-red-multi-sensor"
+
+<a name="installation_in_a_shell"></a>
+### In a shell
+* go to the Node-RED installation folder, e.g.: `~/.node-red`
+* run `npm install node-red-multi-sensor`
+
+<a name="usage"></a>
+## Usage
+
+<a name="node_configuration"></a>
+### Node Configuration
+The node configuration sets the active on/off/toggle keywords as well as optional topic names.
+
+![node-settings](assets/node-settings.png "Node properties")  
+**Fig. 2:** Node properties
+
+#### ON value / OFF value / Toggle value
+These attributes can be of type
+* string
+* number
+* boolean
+
+They are set to the values you want to be the keywords within `msg.payload` when the actions **switch to ON**, **switch to OFF** and **toggle** shall take place (*execution command*).
+
+**Remark:** If you do not set a value to one of the attributes the according method (e.g. switch ON) can not be executed by the node (it needs a value to compare...).
+
+#### On/Off topic resp. Toggle topic (optional)
+If you set the topic attributes to a value, the input `msg` needs to contain the same topic name to execute **On/Off** resp. **toggle** in addition to the `msg.payload` value containing the execution command.
+
+#### Pass through ON/OFF messages
+The node has three pass through modes:  
+ - ***No*** - means that node sends an output `msg` only when state is toggled. ON/OFF input messages just update internal state of the node.
+ - ***If changed*** - sends an output `msg` for ON/OFF input message only once it differs from the previous state. And obviously message is always sent for "toggle" message.
+ - ***Always*** - the output `msg` is always sent as a reaction on an input `msg`.
+
+
+<a name="input"></a>
+### Input
+The input `msg.payload` contains the **execution command** to the node.  
+The value needs to be identical to the values you configured in the configuration dialog.
+
+If a (string) value is set to the configuration attributes ***On/Off topic*** or ***Toggle topic***, the `msg.topic` property must contain the same string value to execute the command given in `msg.payload`.
+
+An example `msg` contents is shown for ***On/Off topic*** = "onofftopic":
+
+![node-settings](assets/topic-usage.png "Use of topics")  
+**Fig. 3:** Example `msg` when using topics
+
+
+<a name="output"></a>
+### Output
+The input `msg` is forwarded to the output, if a valid switch command was detected.  
+The configuration attribute ***pass through ON/OFF messages*** is taken into account.
+
+
+### Node status
+The node status signals:
+- If the switch status is ***on*** it shows a green dot with the text **ON**.
+- If the switch status is ***off*** it shows a red dot with the text **OFF**.
+
+Initially it shows no state.
+
+
+<a name="examples"></a>
+## Examples
+***
+**Remark**: Example flows are present in the examples subdirectory. In Node-RED they can be imported via the import function and then selecting *Examples* in the vertical tab menue.
+***
+
+<a name="example1"></a>
+### Example 1: Basic usage
+This example shows the basic usage.  
+The configuration is set to **boolean** `true` resp. `false` to switch on resp. switch off and **no toggle option**.
+
+<img src="assets/usage-basic.png" title="Example 1" width="550" />
+
+[**BasicUsage.json**](examples/BasicUsage.json)  
+**Fig. 4:** Basic usage example
+
+
+<a name="example2"></a>
+### Example 2: Usage with topics
+This example shows the usage of topics.  
+The configuration is set to **strings** (`switchON`, `switchOFF`) to switch on resp. off and a **string** (`toggleSTATE`) to toggle the switch.  
+Additionally, the topics are set to `onofftopic` (for the commands `switchON` and `switchOFF`) and to `toggletopic` for the toggle operation (command `toggleSTATE`).
+
+<img src="assets/usage-with-topic.png" title="Example 2" width="600" />
+
+[**TopicUsage.json**](examples/TopicUsage.json)  
+**Fig. 5:** Topic usage example
+
+
+
+## Version history
+v0.1.0 Docu rework
+
+v0.0.2 Docs updated
+
+v0.0.1 Initial release
+
+## Credits
+- [eschava](https://github.com/eschava)
+- [StephanStS](https://github.com/StephanStS)
---
+++ multiSensor.js
@@ -0,0 +1,206 @@
+module.exports = function(RED) {
+    var STATUS_INACTIVE = {id:0,name:"inactive"};
+    var STATUS_ACTIVE = {id:1,name:"active"};
+    var STATUS_ACTIVATING = {id:2,name:"activating"};
+    var STATUS_DEACTIVATING = {id:3,name:"deactivating"};
+    var STATUS_CANCEL_ACT = {id:130,name:"abortActivation"};
+    var STATUS_CANCEL_DEACT = {id:131,name:"abortDeactivation"};
+
+    function SensorNode(config) {
+        RED.nodes.createNode(this,config);
+        var node = this;
+
+        if (config.activateDelayUnit === "milliseconds") this.activeDelay = config.activateDelay;
+        else if (config.activateDelayUnit === "minutes") this.activeDelay = config.activateDelay * (60 * 1000);
+        else if (config.activateDelayUnit === "hours")   this.activeDelay = config.activateDelay * (60 * 60 * 1000);
+        else if (config.activateDelayUnit === "days")    this.activeDelay = config.activateDelay * (24 * 60 * 60 * 1000);
+        else this.activeDelay = config.activateDelay * 1000;
+
+        if (config.inactivateDelayUnit === "milliseconds") this.inactiveDelay = config.inactivateDelay;
+        else if (config.inactivateDelayUnit === "minutes") this.inactiveDelay = config.inactivateDelay * (60 * 1000);
+        else if (config.inactivateDelayUnit === "hours")   this.inactiveDelay = config.inactivateDelay * (60 * 60 * 1000);
+        else if (config.inactivateDelayUnit === "days")    this.inactiveDelay = config.inactivateDelay * (24 * 60 * 60 * 1000);
+        else this.inactiveDelay = config.inactivateDelay * 1000;
+        
+        node.activeTimer = -1;
+        node.inactiveTimer = -1;
+
+        var active = false;
+        var sensors = [];
+        var customStatus;
+        var origMsg;
+
+        function get(id) {
+            var s = sensors.find(function(s) { return s.id==id; });
+            if (!s) {
+                s = {id:id, active:false};
+                sensors.push(s);
+            }
+            return s;
+        }
+
+        function countActive() {
+            var c = 0;
+            for (var i=sensors.length-1; i>=0; i--)
+                if (sensors[i].active) c++;
+            return c;
+        }
+
+        function abortActive() {
+            if (node.activeTimer==-1) return;
+            clearInterval(node.activeTimer);
+            node.activeTimer = -1;
+            state(STATUS_CANCEL_ACT);
+        }
+
+        function abortInactive() {
+            if (node.inactiveTimer==-1) return;
+            clearInterval(node.inactiveTimer);
+            node.inactiveTimer = -1;
+            state(STATUS_CANCEL_DEACT);
+        }
+
+        function sendOnActive(msg) {
+            // Abort any inactive timer
+            abortInactive();
+
+            // If already active, then
+            if (active)
+                return;
+            
+            var activeDelay = Number.isInteger(msg.activeDelay) ? msg.activeDelay : node.activeDelay;
+
+            if (activeDelay == 0) {
+                doSend(msg, true);
+            } else {
+                state(STATUS_ACTIVATING);
+                if (node.activeTimer==-1) {
+                    node.activeTimer = setTimeout(function() {
+                        node.activeTimer = -1;
+                        // If not already been customly activated, then send activate
+                        if (!active)
+                            doSend(msg, true);
+                        
+                    }, activeDelay);
+                }
+            }
+        }
+
+        function sendOnInactive(msg) {
+            // Abort any active timer
+            abortActive();
+
+            // If already inactive, then
+            if (!active)
+                return;
+
+            var inactiveDelay = Number.isInteger(msg.inactiveDelay) ? msg.inactiveDelay : node.inactiveDelay;
+        
+            if (inactiveDelay == 0) {
+                doSend(msg, false);
+            } else {
+                state(STATUS_DEACTIVATING);
+                if (node.inactiveTimer==-1) {
+                    node.inactiveTimer = setTimeout(function() {
+                        node.inactiveTimer = -1;
+                        
+                        // If not already been customly deactivated, then send deactivate
+                        if (active)
+                            doSend(msg, false);
+                        
+                    }, inactiveDelay);
+                }
+            }
+        }
+
+        function state(id, skipSend) {
+            if (!skipSend)
+                node.send(config.seperated ? [undefined,undefined,id] : [undefined,id]);
+            return id;
+        }
+
+        function getData(value, type, msg) {
+            switch (type) {
+                case "pay":     return origMsg;
+                case "payl":    return msg;
+                case "nul":     return undefined;
+                default:        return RED.util.evaluateNodeProperty(value, type, node, msg);
+            }
+        }
+
+        function doSend(msg, isActive) {
+            active = isActive;
+            updateStatus();
+
+            if (isActive) {
+                if (config.sendOnActiveType == "nul") return;
+                msg = getData(config.sendOnActive, config.sendOnActiveType, msg);
+            } else {
+                if (config.sendOnInactiveType == "nul") return;
+                msg = getData(config.sendOnInactive, config.sendOnInactiveType, msg);
+            }
+
+            if (config.seperated == true) {
+                node.send([isActive?msg:undefined, isActive?undefined:msg, state(isActive?STATUS_ACTIVE:STATUS_INACTIVE, true)]);
+            }
+            else
+                node.send([msg, state(isActive?STATUS_ACTIVE:STATUS_INACTIVE, true)]);
+        }
+
+        function updateStatus() {
+            try {
+                if (customStatus==undefined) {
+                    var c = active?"green":"red";
+                    if (node.activeTimer!=-1 || node.inactiveTimer!=-1)
+                        c = "yellow";
+                    node.status({fill:c,shape:"dot",text:countActive() + " of " + sensors.length + " sensors active."});
+                } else
+                    node.status(customStatus);
+            } catch(e) {
+            node.warn("Invalid msg.status!");
+            }
+        }
+
+        node.on('input', function(msg) {
+            var id = RED.util.evaluateNodeProperty(config.idField, config.idType, node, msg);
+            var inputValue = RED.util.evaluateNodeProperty(config.inputValue, config.inputType, node, msg);
+            var activeValue = RED.util.evaluateNodeProperty(config.activeValue, config.activeType, node, msg);
+            var inactiveValue = RED.util.evaluateNodeProperty(config.inactiveValue, config.inactiveType, node, msg);
+            
+            var s = get(id);
+
+            if (inputValue == activeValue) {
+                var a = countActive();
+                // If same state, then do nothing
+                if (s.active)
+                    return;
+                
+                s.active = true;
+                // If first active, then
+                if (a==0) {
+                    origMsg = msg;
+                    sendOnActive(msg);
+                }
+                
+            } else if (inputValue == inactiveValue) {
+                // If same state, then do nothing
+                if (!s.active)
+                    return;
+                
+                s.active = false;
+                // If nothing active anymore
+                if (countActive()==0)
+                    sendOnInactive(msg);
+
+            } else {
+                node.warn('Neither active nor inactive');
+            }
+
+            customStatus = msg.status;
+            updateStatus();
+        });
+
+        updateStatus();
+    }
+    RED.nodes.registerType("multiSensor",SensorNode);
+};
